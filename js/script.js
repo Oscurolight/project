233cf2048ce0f8c49dd46f6fc6c2a0f1b0370f23
dima <dmnk0@BlackFlame>
2020-07-20 21:21:41 +0400

    dest 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-let number = 5;
-const leftBorderWidth = 1;
+"use strict";
 
-number = 10;
-console.log(number);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB= {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+
+console.log(personalMovieDB);
