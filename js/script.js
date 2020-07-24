59bafdd741059f920c8d4dec4a155f8d1289de9e
dima <dmnk0@BlackFlame>
2020-07-24 15:54:54 +0400

    dest 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+//'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -10,9 +10,30 @@ const personalMovieDB= {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
+for (let i = 0; i < 2; i++){
+    
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', ''); 
+
+    if (a != null  && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log("Error");   
+        i--;
+    }
+ 
+}
+
+if(personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");    
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if(personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");    
+}
 
 console.log(personalMovieDB);
